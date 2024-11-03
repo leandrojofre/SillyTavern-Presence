131c556cc2a22a06d0bb45cd5a0f0852650c8e5b
Leandro Jofre <leandrotomasjofre@gmail.com>
2024-11-03 19:38:08 -0300

    Revert "Fix - Set 'extensionSettings' value"
    This reverts commit ae45d57832c3f55b1767d589a70367f685edc695.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import { characters, chat, chat_metadata, eventSource, event_types, getCurrentChatId, saveChatDebounced, saveSettingsDebounced } from "../../../../script.js";
-import { groups, is_group_generating, selected_group } from "../../../../scripts/group-chats.js";
+import { chat, chat_metadata, getCurrentChatId, characters, saveChatDebounced, eventSource, event_types, saveSettingsDebounced } from "../../../../script.js";
+import { groups, selected_group, is_group_generating } from "../../../../scripts/group-chats.js";
 import { hideChatMessageRange } from "../../../chats.js";
 import { extension_settings } from "../../../extensions.js";
+import { commonEnumProviders } from "../../../slash-commands/SlashCommandCommonEnumsProvider.js";
 import { SlashCommand } from "../../../slash-commands/SlashCommand.js";
 import { ARGUMENT_TYPE, SlashCommandArgument } from "../../../slash-commands/SlashCommandArgument.js";
-import { commonEnumProviders } from "../../../slash-commands/SlashCommandCommonEnumsProvider.js";
 import { SlashCommandParser } from "../../../slash-commands/SlashCommandParser.js";
 
 const extensionName = "Presence";
 
 const extensionNameLong = `SillyTavern-${extensionName}`;
 const extensionFolderPath = `scripts/extensions/third-party/${extensionNameLong}`;
-const extensionSettings = { ...extension_settings[extensionName] };
+const extensionSettings = extension_settings[extensionName];
 const defaultSettings = {
 	enabled: true,
 	location: "top",
